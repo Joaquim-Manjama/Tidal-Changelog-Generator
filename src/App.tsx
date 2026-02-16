ba042d723b7a3e8cfb90afa8cfd67cd02bb56e45
Joaquim Manjama <melorafaelmanjama@gmail.com>
2026-02-16 23:10:25 +0000

    basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header"
+import { NavLink } from "react-router"
 
 function App() {
 
@@ -7,7 +8,7 @@ function App() {
       <Header />      
       <h1 className="text-6xl font-normal">Track. Generate. Ship.</h1>
       <h1 className="text-ocean-blue-600 font-normal mt-10">Turn chaotic commit history into structured, release-ready changelogs.</h1>
-      <button className="mt-15 bg-black p-5 pl-10 pr-10 rounded-xl bg-sky-surge-600 font-normal hover:bg-sky-surge-700 hover:cursor-pointer">Get Started</button>
+      <NavLink to="/auth" className="mt-15 bg-black p-5 pl-10 pr-10 rounded-xl bg-sky-surge-600 font-normal hover:bg-sky-surge-700">Get Started</NavLink>
     </div>
   )
 }
---
+++ src/pages/Auth.tsx
@@ -0,0 +1,10 @@
+import { NavLink } from "react-router"
+
+const Auth = () => {
+    return <div>
+        <h1>Authentication Page</h1>
+        <NavLink to="/">Go back</NavLink>
+    </div>
+}
+
+export default Auth
