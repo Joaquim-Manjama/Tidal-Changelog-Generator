9ee22c45e73da325ba9db6caad8aeb9c7cd7bba0
Joaquim Manjama <melorafaelmanjama@gmail.com>
2026-05-30 12:49:48 +0100

    improves UX when creating a new project and release

--- src/pages/ProjectSettings.tsx
+++ src/pages/ProjectSettings.tsx
@@ -1,10 +1,23 @@
 import SideBar from "../components/SideBar";
 import Header from "../components/Header";
 import { useUserData } from "../contexts/UserDataContext";
+import { deleteProject } from "../services/Projects";
+import { useNavigate } from "react-router";
 
 const ProjectSettings = () => {
    
     const {currentProject} = useUserData();
+
+    const githubStatus = JSON.parse(sessionStorage.getItem("githubStatus") || "null");
+
+    const navigate = useNavigate();
+    
+    const handleDelete = async () => {
+            
+            const response = await deleteProject(currentProject.id);
+            console.log(response);
+            navigate("/dashboard");
+        }
    
     return <div className="relative p-4 pr-0 w-full min-h-screen flex texture">
             <SideBar/> 
@@ -19,37 +32,32 @@ const ProjectSettings = () => {
                         <span>{`${currentProject.name}`}</span>
                     </p>
                     <p>
-                        <span className="font-medium text-dark-teal-700">Email: </span>
+                        <span className="font-medium text-dark-teal-700">Slug: </span>
                         <span>{`${currentProject.slug}`}</span>
                     </p>
                 </div>
 
-                <h1 className="text-3xl mb-3">Branding</h1>
-                <div className="border rounded-xl p-4 bg-ocean-blue-900 text-gray-200 mb-15">
-                    <p>
-                        <span className="font-medium text-dark-teal-700">Logo: </span>
-                    </p>
-                    <p>
-                        <span className="font-medium text-dark-teal-700">Branding Colour: </span>
-                    </p>
-                </div>
-
                 <h1 className="text-3xl mb-3">GitHub Integration</h1>
                 <div className="border rounded-xl p-4 bg-ocean-blue-900 text-gray-200 mb-15">
-                    <p>
-                        <span className="font-medium text-dark-teal-700">Repository: </span>
-                    </p>
-                    <p>
-                        <span className="font-medium text-dark-teal-700">Branch: </span>
-                    </p>
-                    <div className="flex justify-center gap-20">
-                        <button>Save</button>
-                        <button>Disconect GitHub</button>
-                    </div>
+
+                    {
+                        githubStatus?.connected ?
+                        <p>
+                            <span className="font-medium text-dark-teal-700">Repository: </span>
+                        </p>
+                        :
+                        <button className="bg-gray-800 text-white h-12 pl-2 pr-2 rounded hover:bg-gray-600 hover:cursor-pointer w-70">
+                            Connect to your GitHub Account
+                        </button>
+                    }
                 </div>
 
-                <h1 className="text-3xl mb-3">GitHub Integration</h1>
-                <button>Delete Project</button>
+                <h1 className="text-3xl mt-10 mb-3">Danger Zone</h1>
+                <div className="border rounded-xl p-4 bg-ocean-blue-900 text-gray-200 flex justify-center">
+                    <button onClick={() => handleDelete()} className="bg-red-900 text-white h-12 pl-2 pr-2 rounded hover:bg-red-950 hover:cursor-pointer w-50">
+                        Delete Project
+                    </button>
+                </div>
             </div>
         </div>
 }
