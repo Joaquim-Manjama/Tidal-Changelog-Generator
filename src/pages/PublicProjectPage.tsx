8e038a03067d099400bcf26092c520d0abb60ec6
Joaquim Manjama <melorafaelmanjama@gmail.com>
2026-08-03 22:24:39 +0200

    start releases section

--- src/pages/PublicProjectPage.tsx
+++ src/pages/PublicProjectPage.tsx
@@ -46,10 +46,11 @@ const PublicProjectPage = () => {
                     projectData ? 
                         
                         <div className="flex flex-col w-full h-screen bg-[#f4f8fb]">
+                            {/* Header */}
                             <div className="bg-[#001021] w-full">
 
                                 <div className="w-full flex flex-col border-b-1 border-white/20 p-6 md:px-10 gap-5">
-                                    <p className="text-white/40">Project Overview</p>
+                                    <p className="text-white/40 tracking-wide">Project Overview</p>
                                     
                                     <div className="flex gap-5">
                                         <h1 className="text-4xl md:text-5xl font-medium">{projectData?.name}</h1>
@@ -78,27 +79,38 @@ const PublicProjectPage = () => {
                                         
                                         <div className="flex flex-col">
                                             <p className="text-2xl font-medium">{projectData?.publishedReleases.length}</p>
-                                            <p className="text-white/40 text-xs font-thin">RELEASES</p>
+                                            <p className="text-white/40 text-xs font-thin tracking-wide">RELEASES</p>
                                         </div>
 
                                         <div className="flex flex-col">
                                             <p className="text-2xl font-medium">{totalFeatures}</p>
-                                            <p className="text-white/40 text-xs font-thin">FEATURES</p>
+                                            <p className="text-white/40 text-xs font-thin tracking-wide">FEATURES</p>
                                         </div>
 
                                         <div className="flex flex-col">
                                             <p className="text-2xl font-medium">{totalFixes}</p>
-                                            <p className="text-white/40 text-xs font-thin">FIXES</p>
+                                            <p className="text-white/40 text-xs font-thin tracking-wide">FIXES</p>
                                         </div>
 
                                         <div className="flex flex-col">
                                             <p className="text-2xl font-medium">{totalImprovements}</p>
-                                            <p className="text-white/40 text-xs font-thin">IMPROVEMENTS</p>
+                                            <p className="text-white/40 text-xs font-thin tracking-wide">IMPROVEMENTS</p>
                                         </div>
 
                                 </div>
 
                             </div>
+
+                            {/* Releases*/}
+                            <div className="w-full flex flex-col px-6 md:px-10 py-10">
+
+                                <div className="flex justify-between text-[#3a6880] text-xs tracking-wide">
+                                    <p>PUBLISHED RELEASES</p>
+                                    <p>{projectData?.publishedReleases.length} total</p>
+                                </div>
+
+                            </div>
+
                         </div> 
                     : 
                         <NotFound/>
